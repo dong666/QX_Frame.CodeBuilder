c3120e7ef1f6bfe58df018f53d74bd84600f75cc
qixiao <wd8622088@foxmail.com>
2017-08-17 10:29:54 +0800

    update

--- 10-code/QX_Frame.CodeBuilder/CodeCreate/QX_FrameToDataService.cs
+++ 10-code/QX_Frame.CodeBuilder/CodeCreate/QX_FrameToDataService.cs
@@ -43,16 +43,16 @@ namespace CSharp_FlowchartToCode_DG.CodeCreate
             //}
 
 
-            //添加using
-            str.Append($"{usings}\r\n");
-            str.Append("\r\n");//引用结束换行
-
             #region 版权信息
             //版权信息
             str.Append(Info.CopyRight);
             str.Append("\r\n");
             #endregion
-            
+
+            //添加using
+            str.Append($"{usings}\r\n");
+            str.Append("\r\n");//引用结束换行
+
             //添加命名空间
             str.Append($"namespace {NameSpace}{NameSpaceCommonPlus}\r\n");
             str.Append("{" + "\r\n");
--- 10-code/QX_Frame.CodeBuilder/config/Info.cs
+++ 10-code/QX_Frame.CodeBuilder/config/Info.cs
@@ -11,23 +11,29 @@ namespace CSharp_FlowchartToCode_DG
          * */
 
         //版权信息通用模块
-        public static readonly string VersionNum = "4.2.0";         //版本号
-        public static readonly string Author = "qixiao(柒小)";        //作者
+        public const string VersionNum = "4.2.0";         //版本号
+        public const string Author = "qixiao(柒小)";        //作者
         public static readonly string Description = $"qixiao code builder start at:{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")}";
 
         //设置版权信息
         public static string CopyRight
         {
+            
             get
             {
                 #region 版权信息
                 StringBuilder str = new StringBuilder();    //版本信息
-                str.Append("/**" + "\r\n");
-                str.Append(" * copyright qixiao code builder ->" + "\r\n");
-                str.Append(" * version:" + VersionNum + "\r\n");
-                str.Append(" * author:" + Author + "\r\n");
-                str.Append(" * create:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\r\n");
-                str.Append(" **/" + "\r\n");
+                str.Append("/*********************************************************" + "\r\n");
+                str.Append(" * CopyRight: QIXIAO CODE BUILDER. \r\n");
+                str.Append(" * Version:" + VersionNum + "\r\n");
+                str.Append(" * Author:" + Author + "\r\n");
+                str.Append(" * Create:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\r\n");
+                str.Append(" * E-mail: dong@qixiao.me | wd8622088@foxmail.com \r\n");
+                str.Append(" * Personal WebSit: http://qixiao.me \r\n");
+                str.Append(" * Technical WebSit: http://www.cnblogs.com/qixiaoyizhan/ \r\n");
+                str.Append(" * Description:-.\r\n");
+                str.Append(" * Thx , Best Regards ~\r\n");
+                str.Append(" *********************************************************/" + "\r\n");
                 #endregion
                 return str.ToString(); ;
             }
